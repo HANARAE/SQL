760383101b6a135858b88908d7aba91b5272c4fa
HANARAE <1532163@naver.com>
2023-04-13 14:42:34 +0900

    20230413(목) SQL

---
+++ 06-1_집합연산자.sql
@@ -0,0 +1,50 @@
+
+-- 집합 연산자
+-- UNION(합집합 중복x), UNION ALL(합집합 중복o), INTERSECT(교집합), MINUS(차집합)
+-- 위 아래 컬럼 개수와 데이터 타입이 정확하게 일치해야 한다.
+
+SELECT 
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+UNION
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
+
+
+
+SELECT 
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+UNION ALL
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
+
+
+
+SELECT 
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+INTERSECT
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
+
+
+
+SELECT 
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+MINUS
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
