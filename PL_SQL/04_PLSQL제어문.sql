09e13bf438556ac94f0a9c67b8b44632bff953b5
HANARAE <1532163@naver.com>
2023-04-24 18:11:26 +0900

    20230424 PLSQL시작

--- PL_SQL/04_PLSQL제어문.sql
+++ PL_SQL/04_PLSQL제어문.sql
@@ -70,7 +70,32 @@ END;
 
 
 
-
+-- 중첩 if문
+DECLARE
+    v_salary NUMBER := 0;
+    v_department_id NUMBER := 0;
+    v_commission NUMBER := 0;
+BEGIN
+    v_department_id := ROUND(DBMS_RANDOM.VALUE(10, 120), -1);
+    
+    SELECT
+        salary, commission_pct
+    INTO
+        v_salary, v_commission
+    FROM employees
+    WHERE department_id = v_department_id
+    AND ROWNUM = 1; -- 첫째 값만 구해서 변수에 저장하기 위해
+    
+    dbms_output.put_line(v_salary);
+    
+    IF v_commission > 0 THEN
+        IF v_commission > 0.15 THEN
+            dbms_output.put_line(v_salary * v_commission);
+        END IF;
+    ELSE
+        dbms_output.put_line(v_salary);
+    END IF;
+END;
 
 
 
